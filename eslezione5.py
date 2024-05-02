41d5002431a3f74cdcc58d2197013b234caf036d
lorenzozannini <lorenzozannini04@gmail.com>
2024-05-02 18:06:33 +0200

    es

---
+++ eslezione5.py
@@ -0,0 +1,10 @@
+def create_playlist(name,*song):
+    d={}
+    d[name]=song
+    return d
+dict=create_playlist("Road Trip", {"Song 1", "Song 2"})
+
+def add_like(d,name,liked):
+    
+    return d
+print(add_like(dict,"Road Trip" ,liked=True))
