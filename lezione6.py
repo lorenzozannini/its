3fd66f09904e5ec5ae18b8a56d882e7d9b49410b
lorenzozannini <lorenzozannini04@gmail.com>
2024-05-07 13:31:47 +0200

    kk

--- lezione6.py
+++ lezione6.py
@@ -1,7 +1,18 @@
+"""
 class Person:
     def __init__(self,name,age):
         self.name=name
         self.age=age
+        self.hobby=[]
+    def __str__(self) -> str:
+        return f"(name={self.name},age={self.age})"
+    def add_hobby(self,hobby):
+        self.hobby.append(hobby)
+    def remove_hobby(self,oldhobby):
+        if oldhobby in self.hobby:
+            self.hobby.remove(oldhobby)
+    def add_more_hobby(self,l):
+        self.hobby+=l
 
 alice=Person("Alice W.",45)
 bob=Person("Bob M.",36)
@@ -15,7 +26,52 @@ marco=Person("Marco M.",86)
 sara=Person("Sara M.",76)
 people=[alice,bob,pippo,marco,sara]
 min=alice.age
-for i in people:
-    if i.age<min:
-        min=i.age
-print(min)
+k=0
+for i in range(0,len(people)):
+    if people[i].age<min:
+        min=people[i].age
+        k=i
+people[k].add_hobby("ciao")
+print(people[k].hobby)
+people[k].add_more_hobby(["calcio","auto","sale"])
+print(people[k].hobby)
+people[k].remove_hobby("ciao")
+print(people[k].hobby)
+
+class Student:
+    def __init__(self,name,studyProgram,age,gender):
+        self.name=name
+        self.studyProgram=studyProgram
+        self.age=age
+        self.gender=gender
+
+    def printInfo(self):
+        print(f"(Name={self.name}, Study program={self.studyProgram}, Age={self.age}, Gender={self.gender})")
+
+s1=Student("Lorenzo",10,19,"M")
+s2=Student("Davide",7,19,"M")
+s3=Student("Munir",9,23,"M")
+s3.printInfo()"""
+
+class Animals:
+    def __init__(self,name,legs):
+        self.name=name
+        self.legs=legs
+    def setLegs(self,n):
+        self.legs=n
+    def getLegs(self):
+        return self.legs
+    def printInfo(self):
+        print(f"(Name={self.name}, Legs={self.legs})")
+
+
+cat=Animals("Cat",4)
+dog=Animals("Dog",4)
+print(cat.name)
+print(dog.name)
+cat.legs=5
+print(cat.legs)
+cat.setLegs(7)
+print(cat.legs)
+print(cat.getLegs())
+cat.printInfo()
