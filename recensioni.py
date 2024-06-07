94397b72ac200196cfdcd787fbe2807561d54e91
lorenzozannini <lorenzozannini04@gmail.com>
2024-06-07 17:47:59 +0200

    kk

--- recensioni.py
+++ recensioni.py
@@ -9,11 +9,11 @@ class Media:
     def get_title(self):
         return self.title
     
-    def set_reviews(self,voto):
-        if voto<=5:
+    def add_reviews(self,voto):
+        if voto<=5 and voto>=1:
             self.reviews.append(voto)
         else:
-            print("Voto non valido")
+            print("Voto ",voto,"non valido")
         
     def get_media(self):
         return (sum(self.reviews)/len(self.reviews))
@@ -21,22 +21,61 @@ class Media:
     def get_rate(self):
         rate=round((sum(self.reviews)/len(self.reviews)))
         if rate==1:
-            print("Terribile")
+            return("Terribile")
         elif rate ==2:
-            print("Brutto")
+            return("Brutto")
         elif rate ==3:
-            print("Normale")
+            return("Normale")
         elif rate ==4:
-            print("Bello")
+            return("Bello")
         elif rate ==5:
-            print("Grandioso")
+            return("Grandioso")
 
     def rate_percentage(self,voto):
         n=0
-        if voto in self.reviews:
-            for v in self.reviews:
-                if v==voto:
-                    n+=1
+        for v in self.reviews:
+            if v==voto:
+                n+=1
         return (100/len(self.reviews)*n)
-    
-    def
+        
+    def recensione(self):
+        s=f"Titolo del Film: {self.get_title()}\n"
+        s+=f"Voto Medio: {self.get_media()}\n"
+        s+=f"Giudizio: {self.get_rate()}\n"
+        s+=f"Terribile: {self.rate_percentage(1)}%\n"
+        s+=f"Brutto: {self.rate_percentage(2)}%\n"
+        s+=f"Normale: {self.rate_percentage(3)}%\n"
+        s+=f"Bello: {self.rate_percentage(4)}%\n"
+        s+=f"Grandioso: {self.rate_percentage(5)}%\n"
+        return s
+
+
+
+
+film=Media()
+film.set_title("Cars")
+film.add_reviews(1)
+film.add_reviews(2)
+film.add_reviews(3)
+film.add_reviews(4)
+film.add_reviews(4)
+film.add_reviews(4)
+film.add_reviews(5)
+film.add_reviews(5)
+film.add_reviews(5)
+film.add_reviews(5)
+print(film.recensione())
+
+film2=Media()
+film2.set_title("Titanic")
+film2.add_reviews(0)
+film2.add_reviews(9)
+film2.add_reviews(2)
+film2.add_reviews(2)
+film2.add_reviews(4)
+film2.add_reviews(2)
+film2.add_reviews(3)
+film2.add_reviews(5)
+film2.add_reviews(5)
+film2.add_reviews(5)
+print(film2.recensione())
