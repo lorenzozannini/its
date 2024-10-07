37aa201cd9990289fe5476b55e31647cbdc8bc6e
lorenzozannini <lorenzozannini04@gmail.com>
2024-10-07 19:24:48 +0200

    kk

--- mancini/cielo.sql
+++ mancini/cielo.sql
@@ -1,24 +1,31 @@
-#7
-select a.comp
-from arrpart a,luogoaeroporto lp, luogoaeroporto la
-where a.partenza=lp.aeroporto
-    and a.arrivo=la.aeroporto
-    and lp.citta='Roma'
-    and la.citta='New York';
+#1
+SELECT a.codice,a.nome, COUNT(DISTINCT ap.comp)
+FROM ArrPart ap,Aeroporto a
+WHERE 
+    a.codice=ap.partenza or
+    a.codice=ap.arrivo
+GROUP BY a.codice,a.nome
 
-#8
-select distinct ar.codice,ar.nome,l.citta,l.nazione
-from arrpart a,luogoaeroporto l,aeroporto ar
-where a.partenza=ar.codice
-    and l.aeroporto=ar.codice
-    and a.comp='MagicFly';
+#2
+SELECT count(*)
+FROM ArrPart ap,Volo v
+WHERE ap.codice=v.codice AND ap.comp=v.comp
+    AND ap.partenza='HTR'
+    AND v.durataMinuti>=100
 
-#9
-select a.codice,a.comp,lp.aeroporto,la.aeroporto
-from arrpart a,luogoaeroporto lp, luogoaeroporto la
-where a.partenza=lp.aeroporto
-    and a.arrivo=la.aeroporto
-    and lp.citta='Roma'
-    and la.citta='New York';
+#5
+SELECT a.codice,MIN(c.annoFondaz) min_anno_fondaz
+FROM Aeroporto a,ArrPart ap,Compagnia c
+WHERE 
+    a.codice=ap.partenza OR
+    a.codice=ap.arrivo
+    AND ap.comp=c.nome
+GROUP BY a.codice
 
-#10
+
+#6
+SELECT lap.nazione, COUNT(DISTINCT laa.nazione)
+FROM LuogoAeroporto lap,ArrPart ap,LuogoAeroporto laa
+WHERE lap.aeroporto=ap.partenza
+    AND laa.aeroporto=ap.arrivo
+GROUP BY lap.nazione
