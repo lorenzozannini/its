843cc1e89b231aa325e005b382c7745b18f565c9
lorenzozannini <159887488+lorenzozannini@users.noreply.github.com>
2024-10-16 12:30:37 +0200

    Add files via upload

---
+++ java1/P20241016_01_Autoveicoli/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Autoveicolo autoveicolo1 = new Autoveicolo(1600, 15, 50, "Rosso", 20000);
+		Autoveicolo autoveicolo2 = new Autoveicolo(2000, 12, 55, "Blu", 25000);
+		Autoveicolo autoveicolo3 = new Autoveicolo(1400, 18, 45, "Nero", 18000);
+
+		System.out.println(autoveicolo1.CalcolaPercorrenza(3));
+		System.out.println(autoveicolo1.CalcolaPercorrenza(11));
+		System.out.println(autoveicolo2.CalcolaPercorrenza(3));
+		System.out.println(autoveicolo2.CalcolaPercorrenza(11));
+		System.out.println(autoveicolo3.CalcolaPercorrenza(3));
+		System.out.println(autoveicolo3.CalcolaPercorrenza(11));
+
+	}
+
+}
